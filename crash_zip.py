9a3a5328853a2a3c94be8c675c9a9e0814effc78
root <chongbin@eastmoney.com>
2020-10-10 16:09:01 +0800

    update

--- crash_zip.py
+++ crash_zip.py
@@ -1,26 +1,23 @@
-__author__ = 'rocky'
+# -*- coding: utf-8 -*-
 import zipfile
 
-filename="sample.zip"
+def getPw(filename,dictFile):
+    password = open(dictFile, 'r')
+    for p in password:
+        zf = zipfile.ZipFile(filename)
+        p = p.strip('\n')
+        try:
+            zf.extractall("./sample", pwd=p)
+            print "crash. Password is %s" % p
+            exit(0)
+        except:
+            pass
 
-dictFile="pwdict.txt"
-
-password=open(dictFile,'r')
-
-for p in password:
-
-    zf=zipfile.ZipFile(filename)
-
-    p=p.strip('\n')
-
-    try:
-        zf.extractall("./sample",pwd=p)
-
-        print "crash. Password is %s" %p
-
-        exit(0)
-    except:
-        pass
+if __name__ == '__main__':
+    # filename：需要解压的文件；dictFile：字典
+    filename="sample.zip"
+    dictFile="pwdict.txt"
+    getPw(filename, dictFile)
 
 
 
